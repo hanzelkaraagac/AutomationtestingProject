25fc32b839d79a9af6ca0d24696efb2ea820db24
hanzelkaraagac <hanzelgamess@gmail.com>
2023-07-23 22:49:59 +0300

    Arrivals Images Description

--- src/test/java/comautomationtesting/tests/homepage/TC04_Arrivals_Images_Description.java
+++ src/test/java/comautomationtesting/tests/homepage/TC04_Arrivals_Images_Description.java
@@ -9,30 +9,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC04_Arrivals_Images_Description {
-    Homepage homepage = new Homepage();
-    JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
 
     @Test
     public void Arrivals_Images_Description() {
+
+        Homepage homepage = new Homepage();
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
 
         //3) Click on Shop Menu
-
         ReusableMethods.clickWithJS(homepage.shopButton);
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitAndClick(homepage.shopButton, 2);
 
         //4) Now click on Home menu bu tton
-
         homepage.homeButton.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitAndClick(homepage.homeButton, 3);
 
         //5) Test whether the Home page has Three Arrivals only
         //6) The Home page must contain only three Arrivals
-        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
         jsexecutor.executeScript("window.scrollBy(0,1000)");
         int actualArrivals = homepage.threeArrivalOnly.size();
         int expectedArrivals = 3;
@@ -46,12 +45,10 @@ public class TC04_Arrivals_Images_Description {
 
         //8) Test whether it is navigating to next page where the user can add that book into his basket.
         //9) Image should be clickable and should navigate to next page where user can add that book to his basket
-
         Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains(imageText));
         assert homepage.addToBasketButton.isDisplayed();
 
         //10) Click on Description tab for the book you clicked on.
-
         jsexecutor.executeScript("window.scrollBy(0,750)");
         ReusableMethods.waitAndClick(homepage.descriptionButton, 2);
 
