4aca28927c4bdc837bd17a936a11d7bfef19e476
hanzelkaraagac <hanzelgamess@gmail.com>
2023-08-01 18:03:07 +0300

    Remove

--- src/test/java/comautomationtesting/tests/homepage/TC11_Remove.java
+++ src/test/java/comautomationtesting/tests/homepage/TC11_Remove.java
@@ -33,7 +33,6 @@ public class TC11_Remove {
         //6) The Home page must contains only three Arrivals
 
         //7) Now click the image in the Arrivals
-
         homepage.thirdArrivals.click();
 
         //8) Test whether it is navigating to next page where the user can add that book into his basket.
