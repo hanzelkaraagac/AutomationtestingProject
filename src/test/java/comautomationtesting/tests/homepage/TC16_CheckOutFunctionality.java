4184c6ecd299ac7c1338777e6ffe24c0f5420f00
hanzelkaraagac <hanzelgamess@gmail.com>
2023-08-01 18:26:15 +0300

    CheckOutFunctionality

---
+++ src/test/java/comautomationtesting/tests/homepage/TC16_CheckOutFunctionality.java
@@ -0,0 +1,88 @@
+package comautomationtesting.tests.homepage;
+
+import comautomationtesting.pages.Homepage;
+import comautomationtesting.utilities.ConfigurationReader;
+import comautomationtesting.utilities.Driver;
+import comautomationtesting.utilities.ReusableMethods;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC16_CheckOutFunctionality {
+
+    @Test
+    public void CheckOutFunctionality() {
+
+        Homepage homepage = new Homepage();
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+        String imageText;
+
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
+        //3) Click on Shop Menu
+        ReusableMethods.clickWithJS(homepage.shopButton);
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.shopButton, 2);
+
+        //4) Now click on Home menu button
+        homepage.homeButton.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.homeButton, 3);
+
+        //5) Test whether the Home page has Three Arrivals only
+        //6) The Home page must contain only three Arrivals
+        jsexecutor.executeScript("window.scrollBy(0,1000)");
+        int actualArrivals = homepage.threeArrivalOnly.size();
+        int expectedArrivals = 3;
+        Assert.assertEquals(actualArrivals, expectedArrivals);
+
+        //7) Now click the image in the Arrivals
+        imageText = homepage.itemInImage1.getText().toLowerCase();
+        homepage.image1.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(homepage.image1, 5);
+
+        //8) Test whether it is navigating to next page where the user can add that book into his basket.
+        //9) Image should be clickable and should navigate to next page where user can add that book to his basket
+        Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains(imageText));
+        assert homepage.addToBasketButton.isDisplayed();
+
+        //10) Click on the Add To Basket button which adds that book to your basket
+        homepage.addToBasketButton.click();
+        String expectedAddedMessage = "“Selenium Ruby” has been added to your basket.";
+        String actualAddedMessage = homepage.messageText.getText();
+        System.out.println("actualAddedMessage = " + actualAddedMessage);
+        Assert.assertTrue(actualAddedMessage.contains(expectedAddedMessage));
+
+        //11) User can view that Book in the Menu item with price.
+        homepage.viewBasketButton.click();
+        Assert.assertEquals(homepage.productName.getText().toLowerCase(),imageText);
+        Assert.assertTrue(homepage.productPrice.isDisplayed());
+
+        //12) Now click on Item link which navigates to proceed to check out page.
+        homepage.itemLink.click();
+
+        //13) Now user can find total and subtotal values just above the Proceed to Checkout button.
+        Assert.assertTrue(homepage.subtotalValue.isDisplayed());
+        Assert.assertTrue(homepage.finalTotalPrice.isDisplayed());
+
+        //14) The total always < subtotal because taxes are added in the subtotal
+        double subtotal = Double.parseDouble(homepage.subtotalValue.getText().substring(1));
+        double totalPrice= Double.parseDouble(homepage.finalTotalPrice.getText().substring(1));
+        Assert.assertTrue(totalPrice<subtotal,"the step is written wrong.Total price is greater than subtotal");
+
+        //15) Total
+        System.out.println("Total Price= "+ homepage.finalTotalPrice.getText());
+
+
+        //15) Now click on Proceed to Check out button which navigates to payment gateway page.
+        jsexecutor.executeScript("window.scrollBy(0,250)");
+        homepage.proceedToCheckout.click();
+
+        //16) Clicking on Proceed to Checkout button leads to payment gateway page
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Checkout"));
+
+    }
+}
