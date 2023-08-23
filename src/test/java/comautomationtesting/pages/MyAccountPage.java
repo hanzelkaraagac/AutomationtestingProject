8ab1e578f596784ae67af3a6984e78dec98622d8
hanzelkaraagac <hanzelgamess@gmail.com>
2023-08-23 20:53:45 +0300

    MyAccountPage

--- src/test/java/comautomationtesting/pages/MyAccountPage.java
+++ src/test/java/comautomationtesting/pages/MyAccountPage.java
@@ -45,4 +45,97 @@ public class MyAccountPage {
 
     @FindBy(xpath = "//a[.='Dashboard']")
     public WebElement dashboard;
+
+    @FindBy(xpath = "//a[.='My Account']")
+    public WebElement myAccountBtn;
+
+    @FindBy(id = "username")
+    public WebElement emailInputBox;
+    @FindBy(id = "password")
+    public WebElement passInputBox;
+
+    @FindBy(xpath = "//input[@value='Login']")
+    public WebElement loginBtn;
+    @FindBy(xpath = "//a[.='Dashboard']")
+    public WebElement dashboardBtn;
+    @FindBy(xpath = "//ul[@class='woocommerce-error']/li")
+    public WebElement loginErrorMessage;
+
+    @FindBy(xpath = "//label[@for='password']/span")
+    public WebElement astrixInPasswordField;
+
+    @FindBy(xpath = "//a[.='Sign out']")
+    public WebElement signOutBtn;
+
+    @FindBy(xpath = "//h2[.='Login']")
+    public WebElement loginPage;
+
+    @FindBy(xpath = "//a[.='Home']")
+    public WebElement homeButton;
+
+    @FindBy(id = "reg_email")
+    public WebElement regEmailInputBox;
+
+    @FindBy(id = "reg_password")
+    public WebElement regPassInputBox;
+
+    @FindBy(xpath = "//input[@name='register']")
+    public WebElement regBtn;
+
+    @FindBy(xpath = "//a[.='Orders']")
+    public WebElement ordersBtn;
+
+    @FindBy(xpath = "//table[@class='woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table']")
+    public WebElement orderTable;
+    @FindBy(xpath = "//a[.='View']")
+    public WebElement viewBtn;
+
+    @FindBy(xpath = "//h2[.='Order Details']")
+    public WebElement orderDetails;
+    @FindBy(xpath = "//h3[.='Billing Address']")
+    public WebElement billingDetails;
+    @FindBy(xpath = "//h2[.='Customer Details']")
+    public WebElement customerDetails;
+
+    @FindBy(xpath = "//mark[@class='order-number']")
+    public WebElement orderNumber;
+
+    @FindBy(xpath = "//mark[@class='order-date']")
+    public WebElement orderDate;
+
+    @FindBy(xpath = "//mark[@class='order-status']")
+    public WebElement orderStatus;
+    @FindBy(xpath = "//a[.='Addresses']")
+    public WebElement addressesBtn;
+
+    @FindBy(xpath = "//h3[.='Shipping Address']")
+    public WebElement shippingAddress;
+    @FindBy(xpath = "//a[@href='https://practice.automationtesting.in/my-account/edit-address/billing']")
+    public WebElement billingAddressEditBtn;
+
+    @FindBy(xpath = "//a[@href='https://practice.automationtesting.in/my-account/edit-address/shipping']")
+    public WebElement shippingAddressEditBtn;
+
+    @FindBy(xpath = "//input[@name='save_address']")
+    public WebElement saveAddresBtn;
+
+    @FindBy(xpath = "//div[@class='woocommerce-message']")
+    public WebElement addressUpdatedMessage;
+
+    @FindBy(id = "billing_last_name")
+    public WebElement billingLastName;
+
+    @FindBy(id = "shipping_last_name")
+    public WebElement shippingAddressLastName;
+    @FindBy(xpath = "//a[.='Account Details']")
+    public WebElement accountDetailsBtn;
+
+    @FindBy(id = "password_1")
+    public WebElement newPassInputBox;
+
+    @FindBy(xpath = "//a[.='Logout']")
+    public WebElement logoutBtn;
+
+    @FindBy(xpath = "//div[@class='woocommerce-MyAccount-content']")
+    public WebElement myAccountContent;
 }
---
+++ src/test/java/comautomationtesting/tests/myaccount/TC_44_MyAccounts.java
@@ -0,0 +1,44 @@
+package comautomationtesting.tests.myaccount;
+
+import comautomationtesting.pages.MyAccountPage;
+import comautomationtesting.utilities.ConfigurationReader;
+import comautomationtesting.utilities.Driver;
+import comautomationtesting.utilities.ReusableMethods;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_44_MyAccounts {
+
+    @Test
+    public void Dashboard() {
+
+        MyAccountPage myAccountPage = new MyAccountPage();
+
+        //    1) Open the browser
+        //   2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
+        //   3) Click on My Account Menu
+        myAccountPage.myAccountBtn.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitAndClick(myAccountPage.myAccountBtn, 3);
+
+        //   4) Enter registered username in username text box
+        myAccountPage.emailInputBox.sendKeys(ConfigurationReader.getProperty("email"));
+
+        //   5) Enter password in password text box
+        myAccountPage.passInputBox.sendKeys(ConfigurationReader.getProperty("password"));
+
+        //   6) Click on login button
+        myAccountPage.loginBtn.click();
+
+        //7) User must successfully login to the web page
+        Assert.assertTrue(myAccountPage.myAccountContent.isDisplayed());
+
+
+        //8) Click on Myaccount link which leads to Dashboard
+        myAccountPage.myAccountBtn.click();
+
+        //9) User must view Dashboard of the site
+    }
+}
