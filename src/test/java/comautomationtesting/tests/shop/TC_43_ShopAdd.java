524a9f4f6475aa5468121f2ddfcb0393447d8b73
hanzelkaraagac <hanzelgamess@gmail.com>
2023-08-23 20:51:20 +0300

    ShopAdd

--- src/test/java/comautomationtesting/tests/shop/TC_43_ShopAdd.java
+++ src/test/java/comautomationtesting/tests/shop/TC_43_ShopAdd.java
@@ -14,38 +14,42 @@ public class TC_43_ShopAdd {
     public void ShopAdd() {
 
         ShopPage shopPage = new ShopPage();
-        //43. Shop-Add to Basket-View Basket-Tax Functionality
+
         //    1) Open the browser
         //   2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
         //3) Click on Shop Menu
         shopPage.shopBtn.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitAndClick(shopPage.shopBtn, 3);
+
         //4) Click on the Add To Basket button which adds that book to your basket
         ReusableMethods.clickWithJS(shopPage.shopPageAddToBasketBtn);
+
         //5) User can view that Book in the Menu item with price .
         String itemText = shopPage.itemText.getText();
         Assert.assertEquals(itemText, "1 Item");
         String priceText = shopPage.priceText.getText();
         Assert.assertEquals(priceText, "₹500.00");
+
         //6) Now click on Item link which navigates to proceed to check out page.
         ReusableMethods.clickWithJS(shopPage.viewBasketButton);
+
         //7) Now user can find total and subtotal values just above the Proceed to Checkout button.
         Driver.getDriver().navigate().refresh();
         Assert.assertTrue(shopPage.subtotalValue.isDisplayed() && shopPage.finalTotalPrice.isDisplayed());
+
         //8) The total always < subtotal because taxes are added in the subtotal
         double subtotal = Double.parseDouble(shopPage.subtotalValue.getText().substring(1));
         double totalPrice = Double.parseDouble(shopPage.finalTotalPrice.getText().substring(1));
         Assert.assertTrue(totalPrice > subtotal);
+
         //9) The tax rate variers for India compared to other countries
         ReusableMethods.clickWithJS(shopPage.checkoutBtn);
-
         double taxForIndia = Double.parseDouble(shopPage.taxForIndia.getText().substring(1));
         double totalAtLastPage = Double.parseDouble(shopPage.totalAtLastPage.getText().substring(1));
-
         Assert.assertEquals((totalAtLastPage * 2) / 100, taxForIndia);
-
 
         //10) Tax rate for indian should be 2% and for abroad it should be 5%
         shopPage.billingCountryDropdown.click();
@@ -54,6 +58,8 @@ public class TC_43_ShopAdd {
         ReusableMethods.wait(10);
         double taxForotherCountries = Double.parseDouble(shopPage.taxForOtherCountries.getText().substring(1));
         Assert.assertEquals((totalAtLastPage * 5) / 100, taxForotherCountries);
+
+        Driver.closeDriver();
 
     }
 }
