692e1dc3db074278a302a3c56a361e0f02bffcf6
hanzelkaraagac <hanzelgamess@gmail.com>
2023-08-23 20:40:14 +0300

    ShopSale

--- src/test/java/comautomationtesting/tests/shop/TC40_ShopSale.java
+++ src/test/java/comautomationtesting/tests/shop/TC40_ShopSale.java
@@ -18,13 +18,18 @@ public class TC40_ShopSale {
         //    1) Open the browser
         //   2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("au_url"));
+
         //3) Click on Shop Menu
         shopPage.shopBtn.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitAndClick(shopPage.shopBtn, 3);
+
         //4) Click on Sale written product in home page]
         shopPage.saleBtn.click();
+
         //5) User can clearly view the actual price with old price stricken for the sale written products
         Assert.assertTrue(shopPage.actualPrice.isDisplayed() && shopPage.oldPrice.isDisplayed());
+
+        Driver.closeDriver();
     }
 }
